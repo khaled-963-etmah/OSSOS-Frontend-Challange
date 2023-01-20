06ed3f348a2b409d62cc7df246481a597341b082
khaled.312.etmah <khaled.312.etmah@gmail.com>
2023-01-20 15:26:57 +0300

    الحل v1.0.1

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -3,15 +3,32 @@ import localApi from "../api/Data";
 import UserCard from "./userCard";
 
 export default function Users() {
-
-//....................states....................................
+  //....................states....................................
 
   const [searchTerm, setSearchTerm] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(true);
 
- //..................... usseEffect ............................. 
+  //..................... usseEffect .............................
 
   useEffect(() => {
+    // you can use one of them to load data
+    // just uncomment what you want to use and comment other one
+
+    //...........................from some resource.................
+
+    // 1- this function load data using a read API call to some resource.
+
+    // getFilteredSuggestionsFromApi(searchTerm)
+    //   .then(setSuggestions)
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    //...........................from JSON...........................
+
+    // 2- this function load data using a read API call to mock JSON File.
+
     getFilteredSuggestions(searchTerm)
       .then(setSuggestions)
       .catch((error) => {
@@ -19,17 +36,17 @@ export default function Users() {
       });
   }, [searchTerm]);
 
- //..................... handlers................................ 
+  //..................... handlers................................
 
- // to handle search input changes
+  // to handle search input changes
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
-// to handle click on cards :do something
+  // to handle click on cards :do something
   const handleSelect = (suggestion) => {
     setSearchTerm(suggestion);
   };
-//...............................................................
+  //...............................................................
   return (
     <div className="users-con">
       <input
@@ -40,12 +57,14 @@ export default function Users() {
         placeholder="Search"
       />
       <div className="users">
-        {suggestions.length > 0 ? (
+        {loading ? (
+          <div className="no-users"> loading... </div>
+        ) : suggestions.length > 0 ? (
           suggestions.map((suggestion, index) => {
             // if there are suggestion then,
-            // we will render them to usere 
+            // we will render them to usere
             // first  highlight the matching part of the text
-            // then use UserCard Component to render user data 
+            // then use UserCard Component to render user data
             const searchIndex = suggestion.name
               .toLowerCase()
               .indexOf(searchTerm.toLowerCase());
@@ -73,10 +92,10 @@ export default function Users() {
             );
           })
         ) : (
-            // if no suggestion
+          // if no suggestion
           <div className="no-users">
             <h4 className="no-users-h4">
-              we can not foud any user with this namer
+              No user with this name could be found
             </h4>
           </div>
         )}
@@ -84,16 +103,45 @@ export default function Users() {
     </div>
   );
   // this funnction use for fetching data from JSON file on the Same server!!
-  // and return just matched suggestions 
+  // and return just matched suggestions
   async function getFilteredSuggestions(searchTerm) {
     const data = await localApi
       .then((data) => {
+        setLoading(false);
         return data;
       })
       .catch((error) => {
         console.error(error);
       });
     return data.suggestions.filter((suggestion) =>
+      suggestion.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+  // this funnction use for fetching data from some resource
+  // and return just matched suggestions
+  async function getFilteredSuggestionsFromApi(searchTerm) {
+    const data = await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "get",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        let d = data.map((d) => {
+          return {
+            id: d.id,
+            name: d.name,
+            companyName: d.company.name,
+            img: "./img/boy2.webp",
+          };
+        });
+        setLoading(false);
+        return d;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log(data);
+    return data.filter((suggestion) =>
       suggestion.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
   }
